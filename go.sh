7db8c8adb8b21ac513af051b360dadfaf9c80f55
David McCann <dmccann@fastorientation.com>
2016-05-05 12:57:16 -0400

    moar dotfiles

--- go.sh
+++ go.sh
@@ -2,24 +2,20 @@
 # Bootstrap a new goddamn Mac
 mkdir ~/.bash_profile.d
 mkdir -p ~/Documents/Dev/Projects
+mv ../quickstart ~/Documents/Dev/Projects
+
 mkdir ~/Documents/Dev/Environment
 ln -s ~/Documents/Dev/Projects/quickstart/cheatsheets ~/Documents/cheatsheets
 ln -s ~/Documents/Dev/Projects/quickstart/BashFu ~/Documents/Dev/Projects/BashFu 
-ln -s bash_profile ~/.bash_profile
+
+# dotfiles
+ln -sf ~/Documents/Dev/Projects/quickstart/bash_profile ~/.bash_profile
+ln -sf ~/Documents/Dev/Projects/quickstart/vimrc ~/.vimrc
+ln -sf ~/Documents/Dev/Projects/quickstart/gitconfig ~/.gitconfig
 cp .git-completion.bash ~/
 source ~/.bash_profile
 
-# https://wincent.com/wiki/Git_quickstart
-git config --global user.name "David McCann"
 echo "Email?"
 read email
 git config --global user.email "$email"
-git config --global color.ui auto
 
-# shortcut aliases
-git config --global alias.st status
-git config --global alias.ci commit
-git config --global alias.co checkout
-
-# lata!
-mv ../quickstart ~/Documents/Dev/Projects/
